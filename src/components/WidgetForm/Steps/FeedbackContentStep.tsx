9311f537ae45af2b30a6f83de3a9910a70d96878
Bruno Caneo <brunocaneo3@gmail.com>
2022-05-18 12:53:15 -0300

    Fixing bug

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,40 +1,40 @@
-import { ArrowLeft, Camera } from "phosphor-react";
-import { FormEvent, useState } from "react";
-import { FeedbackType, feedbackTypes } from "..";
-import { api } from "../../../service/api";
-import { CloseButton } from "../../CloseButton";
-import { Loading } from "../../Loading";
-import { ScreenshotButton } from "../ScreenShotButton";
+import { ArrowLeft } from 'phosphor-react';
+import { FormEvent, useState } from 'react';
+import { FeedbackType, feedbackTypes } from '..';
+import { api } from '../../../service/api';
+import { CloseButton } from '../../CloseButton';
+import { Loading } from '../../Loading';
+import { ScreenshotButton } from '../ScreenShotButton';
 
 interface FeedbackContentStepProps {
-  feedbackType: FeedbackType;
-  onFeedbackRestartRequested: () => void;
+  feedbackType: FeedbackType
+  onFeedbackRestartRequested: () => void
   onFeedbackSent: () => void
 }
 
 export function FeedbackContentStep({
-  feedbackType,
-  onFeedbackRestartRequested,
-  onFeedbackSent,
+  feedbackType, onFeedbackRestartRequested, onFeedbackSent
 }: FeedbackContentStepProps) {
-  const [screenshot, setScreenshot] = useState<string | null>(null);
-  const [comment, setComment] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState('')
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
-  const feedbackTypeInfo = feedbackTypes[feedbackType];
-  setIsLoading(true);
+  const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
+
+    setIsSendingFeedback(true);
+
     await api.post('/feedbacks', {
       type: feedbackType,
       comment,
       screenshot
     });
 
-    setIsLoading(false);
+    setIsSendingFeedback(false);
 
-    onFeedbackSent();
+    onFeedbackSent()
   }
 
   return (
@@ -42,25 +42,24 @@ export function FeedbackContentStep({
       <header>
         <button
           type="button"
-          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
           onClick={onFeedbackRestartRequested}
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
         >
           <ArrowLeft weight="bold" className="w-4 h-4" />
         </button>
+
         <span className="text-xl leading-6 flex items-center gap-2">
-          <img
-            src={feedbackTypeInfo.image.source}
-            alt={feedbackTypeInfo.image.alt}
-            className="w-6 h-6"
-          />
+          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
           {feedbackTypeInfo.title}
         </span>
+
         <CloseButton />
       </header>
+
       <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-          placeholder="Conte com detalhes o que está acontecendo"
+          placeholder="Conte com detalhes o que está acontecendo..."
           onChange={event => setComment(event.target.value)}
         />
 
@@ -72,13 +71,13 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={comment.length === 0 || isLoading}
-            className="p-2 bg-brand-500 font-medium rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500"
+            disabled={comment.length === 0 || isSendingFeedback}
+            className="p-2 bg-brand-500 font-medium rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            {isLoading ? <Loading /> : 'Enviar Feedback'}
+            {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
           </button>
         </footer>
       </form>
     </>
-  );
+  )
 }
